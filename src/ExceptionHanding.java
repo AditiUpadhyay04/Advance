eba2f6aced1db4c46e3206c9c6f2f6fd15bc3c7b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-02 22:41:53 +0530

    ..

---
+++ src/ExceptionHanding.java
@@ -0,0 +1,13 @@
+public class ExceptionHanding {
+    public static void main(String[] args) {
+        System.out.println("aditi");
+        try{
+            System.out.println(10/0);
+        }
+        catch(ArithmeticException ae){
+            System.out.println("cannot be divided");
+
+        }
+    }
+
+}
